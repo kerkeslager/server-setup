7563bd396c0432c58f675e4db356a55549b78a3a
David Kerkeslager <kerkeslager@gmail.com>
2022-01-17 11:34:33 -0500

    Fix broken check for user

--- init-root.sh
+++ init-root.sh
@@ -1,7 +1,7 @@
 set -e
 
 # Add a user `admin`
-if id -u admin > /dev/null 2>&1; then
+if ! id -u admin > /dev/null 2>&1; then
   adduser admin
   usermod -aG sudo admin
 fi
@@ -9,7 +9,7 @@ fi
 
 # Allow the `admin` user to log in over ssh
 if [ ! -d /home/admin/.ssh ]; then
-  cp -r ~/.ssh /home/admin
+  cp -r ~/.ssh /home/admin/.ssh
   chown -R admin:admin /home/admin/.ssh
 fi
 
