cdd5e5b26bf8e80f18a43c50cbbeb9e6402c4bed
David Kerkeslager <kerkeslager@gmail.com>
2022-01-17 12:51:42 -0500

    Install gunicorn

--- django.sh
+++ django.sh
@@ -17,18 +17,23 @@ DIRECTORY=$2
 HOST=$3
 
 # Install apt packages
-sudo apt install python3-venv
+sudo apt install -y python3-venv
 
+# Clone GITREPO into DIRECTORY
 if [ ! -d $DIRECTORY ] ; then
   git clone $GITREPO $DIRECTORY
 fi
-
 cd $DIRECTORY
 git pull
 
+# Create a virtualenv and install requirements
 if [ ! -d .env ] ; then
   python3 -m venv .env
 fi
-
 source .env/bin/activate
+pip install --upgrade pip wheel
 pip install -r requirements.txt
+
+# Install gunicorn
+pip install gunicorn
+sudo cp /home/admin/server-setup/django-files/gunicorn.socket /etc/systemd/system/gunicorn.socket
