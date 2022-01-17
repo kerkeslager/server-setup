80de6e400b6a1d674fa0d5d10915230fce4ecade
David Kerkeslager <kerkeslager@gmail.com>
2022-01-17 11:47:43 -0500

    Add and configure UFW

--- init.sh
+++ init.sh
@@ -1,3 +1,12 @@
 set -e
 
+# Some basic making sure we're in the right state
 sudo apt update
+cd /home/admin
+
+# Install apt packages
+sudo apt install ufw
+
+# Configure ufw
+sudo ufw allow OpenSSH
+sudo ufw enable
